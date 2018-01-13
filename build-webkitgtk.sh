7d901b173445c14ba6857aba713775a7e8d3a2d7
Christian Scheurer <git@tinytux.ch>
2018-01-13 13:23:10 +0100

    Build script cleanup
    Signed-off-by: Christian Scheurer <git@tinytux.ch>

--- build-webkitgtk.sh
+++ build-webkitgtk.sh
@@ -2,69 +2,42 @@
 #
 # Build webkitgtk for Raspberry Pi 3
 #
-
-set -o errexit
-
 # git submodule add -b morty http://git.yoctoproject.org/git/poky poky
 # git submodule add -b morty http://git.yoctoproject.org/git/meta-raspberrypi meta-raspberrypi
 # git submodule add -b morty http://git.openembedded.org/meta-openembedded meta-openembedded
 # git submodule add -b morty https://github.com/WebPlatformForEmbedded/meta-wpe meta-wpe
 # git submodule add https://github.com/Igalia/meta-webkit meta-webkit
 
-LOCAL_BUILD_DIR="build-webkitgtk"
+set -o errexit
 
-if [[ -e "${LOCAL_BUILD_DIR}" ]]; then
-    echo "${LOCAL_BUILD_DIR} folder already exists. This script works only after a clean checkout."
-    exit 1
-fi
-
-
-source poky/oe-init-build-env ${LOCAL_BUILD_DIR}
-
-
-LOCAL_SOURCE_MIRROR="/opt/yocto/mirror/"
-if [[ -e "${LOCAL_SOURCE_MIRROR}" ]]; then
-    echo "Using local download mirror: ${LOCAL_SOURCE_MIRROR}"
+function config_append_source_mirror() {
 cat <<EOF >> conf/local.conf
 
 # https://wiki.yoctoproject.org/wiki/How_do_I#Q:_How_do_I_create_my_own_source_download_mirror_.3F
-SOURCE_MIRROR_URL ?= "file://${LOCAL_SOURCE_MIRROR}"
+SOURCE_MIRROR_URL ?= "file://${1}"
 INHERIT += "own-mirrors" 
 BB_GENERATE_MIRROR_TARBALLS = "1" 
 # BB_NO_NETWORK = "1"
 EOF
-else
-    echo "${LOCAL_SOURCE_MIRROR} not found. Local mirror not used."
-fi
+}
 
-
-LOCAL_SSTATE_CACHE="/opt/yocto/sstate/"
-if [[ -e "${LOCAL_SSTATE_CACHE}" ]]; then
-    echo "Using local sstate cache: ${LOCAL_SSTATE_CACHE}"
+function config_append_sstate_cache() {
 cat <<EOF >> conf/local.conf
 
 # https://wiki.yoctoproject.org/wiki/Enable_sstate_cache
-SSTATE_DIR ?= "${LOCAL_SSTATE_CACHE}"
+SSTATE_DIR ?= "${1}"
 EOF
-else
-    echo "${LOCAL_SSTATE_CACHE} not found. sstate cache not used."
-fi
+}
 
-
-LOCAL_DOWNLOADS="/opt/yocto/downloads/"
-if [[ -e "${LOCAL_DOWNLOADS}" ]]; then
-    echo "Using local downloads: ${LOCAL_DOWNLOADS}"
+function config_append_dl_dir() {
 cat <<EOF >> conf/local.conf
 
 # http://www.yoctoproject.org/docs/1.6.1/ref-manual/ref-manual.html#var-DL_DIR
-DL_DIR ?= "${LOCAL_DOWNLOADS}"
+DL_DIR ?= "${1}"
 EOF
-else
-    echo "${LOCAL_SSTATE_CACHE} not found. Local downloads not used."
-fi
+}
 
-
-echo "Adding MACHINE configuration..."
+function config_append_machine() {
 cat <<EOF >> conf/local.conf
 
 # https://github.com/WebPlatformForEmbedded/meta-wpe/wiki/Raspberry-PI
@@ -80,8 +53,9 @@ IMAGE_INSTALL_append = " libgl1"
 # https://lists.yoctoproject.org/pipermail/yocto/2016-April/029641.html
 ENABLE_UART = "1"
 EOF
+}
 
-echo "Adding WLAN configuration..."
+function config_append_wlan() {
 cat <<EOF >> conf/local.conf
 # https://github.com/agherzan/meta-raspberrypi/issues/31#issuecomment-352251597
 DISTRO_FEATURES_append = " wifi"
@@ -89,8 +63,9 @@ IMAGE_INSTALL_append = " linux-firmware-bcm43430 i2c-tools python-smbus bridge-u
 IMAGE_INSTALL_append = " linux-firmware iw"
 
 EOF
+}
 
-echo "Adding some useful tools..."
+function config_append_tools() {
 cat <<EOF >> conf/local.conf
 # epiphany browser
 #IMAGE_INSTALL_append = " libsecret-dev libsecret epiphany"
@@ -102,17 +77,75 @@ IMAGE_INSTALL_append = " vim mc htop wget xeyes xdotool"
 #PACKAGE_EXCLUDE = " pulseaudio gvim gtk-play"
 
 EOF
+}
 
-echo "Adding bitbake-layers..."
-bitbake-layers add-layer ../meta-raspberrypi
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-multimedia
-bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-openembedded/meta-networking
-bitbake-layers add-layer ../meta-webkit
+function config_bitbake_layers() {
+    bitbake-layers add-layer ../meta-raspberrypi
+    bitbake-layers add-layer ../meta-openembedded/meta-oe
+    bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+    bitbake-layers add-layer ../meta-openembedded/meta-python
+    bitbake-layers add-layer ../meta-openembedded/meta-networking
+    bitbake-layers add-layer ../meta-webkit
+}
 
+function create_config() {
+    LOCAL_SOURCE_MIRROR="/opt/yocto/mirror/"
+    if [[ -e "${LOCAL_SOURCE_MIRROR}" ]]; then
+        echo "Using local download mirror: ${LOCAL_SOURCE_MIRROR}"
+        config_append_source_mirror "${LOCAL_SOURCE_MIRROR}"
+    else
+        echo "${LOCAL_SOURCE_MIRROR} not found. Local mirror not used."
+    fi
+
+    LOCAL_SSTATE_CACHE="/opt/yocto/sstate/"
+    if [[ -e "${LOCAL_SSTATE_CACHE}" ]]; then
+        echo "Using local sstate cache: ${LOCAL_SSTATE_CACHE}"
+        config_append_sstate_cache "${LOCAL_SSTATE_CACHE}"
+    else
+        echo "${LOCAL_SSTATE_CACHE} not found. sstate cache not used."
+    fi
+
+    LOCAL_DOWNLOADS="/opt/yocto/downloads/"
+    if [[ -e "${LOCAL_DOWNLOADS}" ]]; then
+        echo "Using local downloads: ${LOCAL_DOWNLOADS}"
+        config_append_dl_dir "${LOCAL_DOWNLOADS}"
+    else
+        echo "${LOCAL_SSTATE_CACHE} not found. Local downloads not used."
+    fi
+
+    echo "Adding MACHINE configuration..."
+    config_append_machine
+    echo "Adding WLAN configuration..."
+    config_append_wlan
+    echo "Adding some useful tools..."
+    config_append_tools
+    echo "Adding bitbake-layers..."
+    config_bitbake_layers
+}
+
+# ############
+# ### MAIN ###
+# ############
+echo "webkitgtk for Raspberry Pi 3 build script"
+echo "~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~"
+
+LOCAL_BUILD_DIR="build-webkitgtk"
+SOURCE_CMD="source poky/oe-init-build-env ${LOCAL_BUILD_DIR}"
+echo "${SOURCE_CMD}"
+${SOURCE_CMD}
+
+LOCAL_CONF_FILE="${LOCAL_BUILD_DIR}/conf/local.conf"
+if [[ -e ${LOCAL_CONF_FILE} ]]; then
+    echo "Using existing configuration: ${LOCAL_CONF_FILE}"
+else
+    echo "Creating new configuration: ${LOCAL_CONF_FILE}"
+    create_config
+fi
+
+BUILD_CMD="bitbake core-image-sato"
 echo "Building..."
-bitbake core-image-sato
+echo "${BUILD_CMD}"
+${BUILD_CMD}
 
 echo "Build done. Write to SD card using the following command:"
 echo
